274075699b629b2e880b9006ce5255034ea759c2
Renzo Castagnino <renzo_tp@outlook.com>
2020-10-12 12:00:28 -0600

    Commit 6 - models VGG16

--- Code/model_VGG16.py
+++ Code/model_VGG16.py
@@ -8,28 +8,34 @@ AUTHOR: Renzo Castagnino
 DATE: September 2020
 """
 
+
+# %% -------------------------------------------Imports ----------------------------------------------------------------
 import time
-# %% ------------------------------------------- IMPORT PACKAGES -------------------------------------------------------
 from pathlib import Path
-import torch
 from PIL import ImageFile
 from torch import nn
 from torch import optim
 from torchvision import datasets, transforms, models
+from matplotlib import pyplot as plt
+import torch
 import warnings
 warnings.filterwarnings("ignore")
 
+
 # %% ----------------------------------- Hyper Parameters --------------------------------------------------------------
 LR = 0.001
-N_EPOCHS = 500
 BATCH_SIZE = 4
 DROPOUT = 0.1
+N_EPOCHS = 10
+STEPS = 0
+RUNNING_LOSS = 0
 
-# %% ------------------------------------------- DATA DIR---------------------------------------------------------------
+
+# %% ------------------------------------------- Data Dir --------------------------------------------------------------
 DATA_DIR = (str(Path(__file__).parents[1]) + '/data/data/')
 
 
-# %% ------------------------------------------- PREPROCESS ------------------------------------------------------------
+# %% ------------------------------------------- Data Preparation-------------------------------------------------------
 train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                        transforms.RandomResizedCrop(224),
                                        transforms.RandomHorizontalFlip(),
@@ -47,10 +53,11 @@ train_data = datasets.ImageFolder(DATA_DIR + '/train', transform=train_transform
 test_data = datasets.ImageFolder(DATA_DIR + '/test', transform=test_transforms)
 
 
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
-testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=64)
 
-# %% ------------------------------------------- MODEL VGG16------------------------------------------------------------
+
+# %% ------------------------------------------- VGG16 Model -----------------------------------------------------------
 model = models.vgg16(pretrained=True)
 print(model)
 
@@ -72,10 +79,10 @@ for device in ['cpu']:
 
     criterion = nn.NLLLoss()
     # Only train the classifier parameters, feature parameters are frozen
-    optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.classifier.parameters(), lr=LR)
     model.to(device)
 
-    for ii, (inputs, labels) in enumerate(trainloader):
+    for ii, (inputs, labels) in enumerate(train_loader):
         print(ii)
         # Move input and label tensors to the GPU
         inputs, labels = inputs.to(device), labels.to(device)
@@ -95,7 +102,7 @@ for device in ['cpu']:
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = models.vgg16(pretrained=True)
 
-# Freeze parameters so we don't backprop through them
+# Freeze parameters so we don't do backpropagation
 for param in model.parameters():
     param.requires_grad = False
 
@@ -116,18 +123,15 @@ model.to(device)
 # %% -------------------------------------- Training Loop --------------------------------------------------------------
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-traininglosses = []
-testinglosses = []
-testaccuracy = []
-totalsteps = []
-epochs = 10
-steps = 0
-running_loss = 0
-print_every = 5
-for epoch in range(epochs):
+training_losses = []
+testing_losses = []
+test_accuracy = []
+total_steps = []
+print_every = 1
+for epoch in range(N_EPOCHS):
     print('training...')
-    for inputs, labels in trainloader:
-        steps += 1
+    for inputs, labels in train_loader:
+        STEPS += 1
         # Move input and label tensors to the default device
         inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
@@ -137,14 +141,14 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item()
+        RUNNING_LOSS += loss.item()
 
-        if steps % print_every == 0:
+        if STEPS % print_every == 0:
             test_loss = 0
             accuracy = 0
             model.eval()
             with torch.no_grad():
-                for inputs, labels in testloader:
+                for inputs, labels in test_loader:
                     inputs, labels = inputs.to(device), labels.to(device)
                     logps = model.forward(inputs)
                     batch_loss = criterion(logps, labels)
@@ -157,33 +161,34 @@ for epoch in range(epochs):
                     equals = top_class == labels.view(*top_class.shape)
                     accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
 
-            traininglosses.append(running_loss / print_every)
-            testinglosses.append(test_loss / len(testloader))
-            testaccuracy.append(accuracy / len(testloader))
-            totalsteps.append(steps)
+            training_losses.append(RUNNING_LOSS / print_every)
+            testing_losses.append(test_loss / len(test_loader))
+            test_accuracy.append(accuracy / len(test_loader))
+            total_steps.append(STEPS)
             print(f"Device {device}.."
-                  f"Epoch {epoch + 1}/{epochs}.. "
-                  f"Step {steps}.. "
-                  f"Train loss: {running_loss / print_every:.3f}.. "
-                  f"Test loss: {test_loss / len(testloader):.3f}.. "
-                  f"Test accuracy: {accuracy / len(testloader):.3f}")
-            running_loss = 0
+                  f"Epoch {epoch + 1}/{N_EPOCHS}.. "
+                  f"Step {STEPS}.. "
+                  f"Train loss: {RUNNING_LOSS / print_every:.3f}.. "
+                  f"Test loss: {test_loss / len(test_loader):.3f}.. "
+                  f"Test accuracy: {accuracy / len(test_loader):.3f}")
+            RUNNING_LOSS = 0
             model.train()
 
 
 # %% -------------------------------------- Plot Training --------------------------------------------------------------
-from matplotlib import pyplot as plt
-plt.plot(totalsteps, traininglosses, label='Train Loss')
-plt.plot(totalsteps, testinglosses, label='Test Loss')
-plt.plot(totalsteps, testaccuracy, label='Test Accuracy')
+plt.plot(total_steps, training_losses, label='Train Loss')
+plt.plot(total_steps, testing_losses, label='Test Loss')
+plt.plot(total_steps, test_accuracy, label='Test Accuracy')
 plt.legend()
 plt.grid()
 plt.show()
 
-# %% -------------------------------------- Save Model -- --------------------------------------------------------------
+# %% -------------------------------------- Save Model -----------------------------------------------------------------
 checkpoint = {
     'parameters': model.parameters,
     'state_dict': model.state_dict()
 }
 
 torch.save(checkpoint, 'gender_vgg.pth')
+
+

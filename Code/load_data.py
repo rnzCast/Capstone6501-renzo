e0d3cfba3ac5a24ada062cc8014abbe4688ea456
Renzo Castagnino <renzo_tp@outlook.com>
2020-11-03 18:52:11 -0700

    Commit 6 - Clean code

--- Code/load_data.py
+++ Code/load_data.py
@@ -4,26 +4,24 @@ import shutil
 from pathlib import Path
 
 DATA_DIR = (str(Path(__file__).parents[1]) + '/data/')
-
 df = pd.read_csv(DATA_DIR + 'imdb/imdb.csv')
-print(df)
 
 for i in df.values:
-    g = i[3]
-    i = i[2]
+    g = i[3]  # Gender
+    i = i[2]  # Path
     path = i.split('/')
     path = path[1]
-    image = cv2.imread(DATA_DIR + '/img/' + path)
+    image = cv2.imread(DATA_DIR + 'img/' + path)
+
     try:
-        image = image[:, :, ::-1]
-        print(image)
-        print(path)
-        print("success")
+        print('copying images...')
         if g == 0.0:
-            shutil.copy(DATA_DIR + '/img/' + path,
-                        DATA_DIR + '/train/img/female')
+            shutil.copy(DATA_DIR + 'img/' + path,
+                        DATA_DIR + 'data/train/female')
         else:
-            shutil.copy(DATA_DIR + '/img/' + path,
-                        DATA_DIR + '/train/img/male')
+            shutil.copy(DATA_DIR + 'img/' + path,
+                        DATA_DIR + 'data/train/male')
     except:
         pass
+
+print('done!')

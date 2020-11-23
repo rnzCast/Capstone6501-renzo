18baafccd3fa1d7ae87f5069884ce29914acaddf
Renzo Castagnino <renzo_tp@outlook.com>
2020-11-23 11:18:52 -0700

    Commit 9.2 - Load data file

--- Code/load_data.py
+++ Code/load_data.py
@@ -3,24 +3,23 @@ import cv2
 import shutil
 from pathlib import Path
 
-DATA_DIR = (str(Path(__file__).parents[1]) + '/data/')
-df = pd.read_csv(DATA_DIR + 'imdb/imdb.csv')
+
+df = pd.read_csv('../csv/celeba_list_attr.csv')
+print(df)
 
 for i in df.values:
-    g = i[3]  # Gender
-    i = i[2]  # Path
-    path = i.split('/')
-    path = path[1]
-    image = cv2.imread(DATA_DIR + 'img/' + path)
-
+    path = i[0]  # i[0] corresponds to the first column in the .csv file, referencing the image name
+    g = i[32]  # i[21] corresponds to the column where the male attribute is.
+    image = cv2.imread('../img_align_celeba/' + str(path))
+    print(image)
     try:
         print('copying images...')
-        if g == 0.0:
-            shutil.copy(DATA_DIR + 'img/' + path,
-                        DATA_DIR + 'data/train/female')
+        if g == 1.0:
+            shutil.copy('../img_align_celeba/' + str(path),
+                        '../data/train/male')
         else:
-            shutil.copy(DATA_DIR + 'img/' + path,
-                        DATA_DIR + 'data/train/male')
+            shutil.copy('../img_align_celeba/' + str(path),
+                        '../data_smile/train/female')
     except:
         pass
 
